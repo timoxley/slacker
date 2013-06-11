1a13bec44e2a785c842612716b7608f6aa9c4873
Tim <secoif@gmail.com>
2013-06-11 20:31:14 +0800

    Remove fails.

--- index.js
+++ index.js
@@ -1,43 +1,5 @@
 var net = require('net')
 var cluster = require('cluster')
-console.dir(cluster)
-cluster._getServer = function(tcpSelf, address, port, addressType, fd, cb) {
-  // This can only be called from a worker.
-  assert(cluster.isWorker);
-
-  // Store tcp instance for later use
-  var key = [address, port, addressType, fd].join(':');
-  serverListeners[key] = tcpSelf;
-
-  // Send a listening message to the master
-  tcpSelf.once('listening', function() {
-    cluster.worker.state = 'listening';
-    sendInternalMessage(cluster.worker, {
-      cmd: 'listening',
-      address: address,
-      port: tcpSelf.address().port || port,
-      addressType: addressType,
-      fd: fd
-    });
-  });
-
-  // Request the fd handler from the master process
-  var message = {
-    cmd: 'queryServer',
-    address: address,
-    port: port,
-    addressType: addressType,
-    fd: fd
-  };
-
-  // The callback will be stored until the master has responded
-  sendInternalMessage(cluster.worker, message, function(msg, handle) {
-    cb(handle);
-  });
-
-};
-
-
 var log = require('debug')('spawn-on-demand')
 
 cluster.on('disconnect', function() {
